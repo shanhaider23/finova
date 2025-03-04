a17f0b277bf0829353c72417b24c3f16c7870ac2
shan-e-haider bukhari <shanhaider23@lenovo-home>
2025-03-04 07:58:02 +0100

    added django app

--- backend/finova_backend/finova_app/models.py
+++ backend/finova_backend/finova_app/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+class Budget(models.Model):
+    name = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(max_length=50)
+    icon = models.CharField(max_length=255, blank=True, null=True)
+    created_by = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+# expenses/models.py
+class Expense(models.Model):
+    name = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+    category = models.CharField(max_length=255)
+    created_at = models.DateTimeField()
+    
+    def __str__(self):
+        return self.name
+
+# expenses/models.py
+class Monthly(models.Model):
+    date = models.DateField()
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=50)
+    category = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_by = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+# expenses/models.py
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=50)
+    date = models.DateField()
+    created_at = models.DateTimeField()
+    created_by = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
